0ab5ca1b09e93b709c8ab538d95e39eba36a571c
sealch <thesermanarm@gmail.com>
2020-04-01 17:08:36 +0300

    add morgan logger

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
