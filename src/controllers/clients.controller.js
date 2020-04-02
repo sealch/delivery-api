abe88ea5e230c0aa791eb8a7dd804ca9e4683de6
sealch <thesermanarm@gmail.com>
2020-04-03 00:10:11 +0300

    add client queries

--- src/controllers/clients.controller.js
+++ src/controllers/clients.controller.js
@@ -1,6 +1,7 @@
 import Client from '../models/clients';
+import Menu from "../models/menus";
 
-async function getClients(req, res) {
+export async function getClients(req, res) {
     try {
         const clients = await Client.findAll({
             attributes: ['id', 'name', 'address']
@@ -12,4 +13,83 @@ async function getClients(req, res) {
     }
 }
 
-exports.getClients = getClients;
+export async function createClient(req, res) {
+    let { name, address } = req.body;
+    try {
+        let newClient = await Client.create({
+            name,
+            address
+        }, {
+            fields: ['name', 'address']
+        });
+        if (newClient) {
+            return res.json({
+                message: 'New client created',
+                data: newClient
+            })
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Something went wrong'
+        })
+    }
+}
+
+export async function getOneClient(req, res) {
+    const { id } = req.params;
+    try {
+        const client = await Client.findOne({
+            where: {
+                id
+            }
+        });
+        res.json(client);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export async function updateClient(req, res) {
+    const { id } = req.params;
+    const { name, address } = req.body;
+    try {
+        const client = await Client.findOne({
+            where: {
+                id
+            }
+        });
+        await client.update({
+            name,
+            address
+        });
+        return res.json({
+            message: 'Client updated',
+            data: client
+        })
+    } catch (e) {
+        console.log(e);
+        res.json({
+            message: 'Something went wrong'
+        })
+    }
+}
+
+export async function deleteClient(req, res) {
+    const { id } = req.params;
+    try {
+        await Client.destroy({
+            where: {
+                id
+            }
+        });
+        res.json({
+            message: 'Client deleted',
+        })
+    } catch (e) {
+        console.log(e);
+        res.json({
+            message: 'Something went wrong'
+        });
+    }
+}
--- src/routes/clients.js
+++ src/routes/clients.js
@@ -1,8 +1,18 @@
 import { Router } from 'express'
 const router = Router();
 
-import { getClients } from '../controllers/clients.controller';
+import {
+    getClients,
+    updateClient,
+    createClient,
+    getOneClient,
+    deleteClient
+} from '../controllers/clients.controller';
 
 router.get('/', getClients);
+router.post('/', createClient);
+router.get('/:id', getOneClient);
+router.put('/:id', updateClient);
+router.delete('/:id', deleteClient);
 
 export default router;
