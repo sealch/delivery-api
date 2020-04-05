80589f67ea99ed953008cd3236835b3e3b142d3d
sealch <thesermanarm@gmail.com>
2020-04-05 11:32:50 +0300

    update hardcoded url

--- config/config.js
+++ config/config.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    url: process.env.DEV_DB_URL || 'postgres://postgres:postgres@localhost:5432/postgres',
+    url: 'postgres://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST
+          + ':' + process.env.DB_PORT + '/' + process.env.DB_DATABASE
+          || 'postgres://postgres:postgres@localhost:5432/postgres',
     dialect: 'postgres',
     operatorsAliases: false
   },
   prod: {
-    url: process.env.PROD_DB_URL,
+    url: 'postgres://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST
+        + ':' + process.env.DB_PORT + '/' + process.env.DB_DATABASE
+        || 'postgres://postgres:postgres@postgres:5432/postgres',
     dialect: 'postgres',
     operatorsAliases: false
   }
