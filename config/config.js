349d626bbac32f145c3ddee631507897fdb88526
sealch <thesermanarm@gmail.com>
2020-04-05 11:52:57 +0300

    fix one liner url

--- config/config.js
+++ config/config.js
@@ -2,16 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    url: 'postgres://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST
-          + ':' + process.env.DB_PORT + '/' + process.env.DB_DATABASE
-          || 'postgres://postgres:postgres@localhost:5432/postgres',
+    url: 'postgres://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_DATABASE || 'postgres://postgres:postgres@localhost:5432/postgres',
     dialect: 'postgres',
     operatorsAliases: false
   },
   prod: {
-    url: 'postgres://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST
-        + ':' + process.env.DB_PORT + '/' + process.env.DB_DATABASE
-        || 'postgres://postgres:postgres@postgres:5432/postgres',
+    url: 'postgres://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_DATABASE || 'postgres://postgres:postgres@postgres:5432/postgres',
     dialect: 'postgres',
     operatorsAliases: false
   }
